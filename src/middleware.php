aa2367d8991d6ec2a1280074197e03744860c770
Tobias <thaegi@gmx.de>
2018-01-08 18:24:12 +0100

    Update dependencies, add authentication logging

--- src/middleware.php
+++ src/middleware.php
@@ -1,6 +1,27 @@
 <?php
 
 $container = $app->getContainer();
+$info = [
+    'PHP_AUTH_USER' => array_key_exists('PHP_AUTH_USER', $_SERVER) ? $_SERVER['PHP_AUTH_USER'] : null,
+    'PHP_AUTH_PW' => array_key_exists('PHP_AUTH_PW', $_SERVER) ? $_SERVER['PHP_AUTH_PW'] : null,
+    'HTTP_AUTHORIZATION' => array_key_exists('HTTP_AUTHORIZATION', $_SERVER) ? $_SERVER['HTTP_AUTHORIZATION'] : null,
+    'REMOTE_ADDR' => array_key_exists('REMOTE_ADDR', $_SERVER) ? $_SERVER['REMOTE_ADDR'] : null,
+    'HTTP_USER_AGENT' => array_key_exists('HTTP_USER_AGENT', $_SERVER) ? $_SERVER['HTTP_USER_AGENT'] : null,
+    'HTTP_X_FORWARDED_FOR' => array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : null,
+    'HTTP_CLIENT_IP' => array_key_exists('HTTP_CLIENT_IP', $_SERVER) ? $_SERVER['HTTP_CLIENT_IP'] : null,
+    'REQUEST_METHOD' => array_key_exists('REQUEST_METHOD', $_SERVER) ? $_SERVER['REQUEST_METHOD'] : null,
+    'QUERY_STRING' => array_key_exists('QUERY_STRING', $_SERVER) ? $_SERVER['QUERY_STRING'] : null,
+    'REQUEST_URI' => array_key_exists('REQUEST_URI', $_SERVER) ? $_SERVER['REQUEST_URI'] : null,
+    'SCRIPT_NAME' => array_key_exists('SCRIPT_NAME', $_SERVER) ? $_SERVER['SCRIPT_NAME'] : null,
+];
+
+if (preg_match('/Basic\s+(.*)$/i', $_SERVER['HTTP_AUTHORIZATION'], $matches))
+{
+    list($name, $password) = explode(':', base64_decode($matches[1]));
+    $info['PHP_AUTH_USER'] = strip_tags($name);
+    $info['PHP_AUTH_PW'] = strip_tags($password);
+}
+
 $pdo = $container->get('db');
 
 $app->add(new \Slim\Middleware\HttpBasicAuthentication([
@@ -12,9 +33,16 @@ $app->add(new \Slim\Middleware\HttpBasicAuthentication([
         "table" => "users",
         "user" => "login",
         "hash" => "password"
-            ]),
-    "error" => function ($request, $response, $arguments) use ($container) {
-    
+    ]),
+    "callback" => function ($request, $response, $arguments) use ($container, $info){
+        $logger = $container->get('logger');
+        $logger->addInfo('SITE CALL', $info);
+    },
+    "error" => function ($request, $response, $arguments) use ($container, $info) {
+        
+        $logger = $container->get('logger');
+        $logger->addInfo('Login FAILED', $info);
+        
         return $container['view']->render($response, 'error.twig', ["message" => $arguments["message"], "message_type" => "danger"]);
     }
 ]));
