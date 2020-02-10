ba6d9c514564ca7ecaa324c29b6cf957b1edc9b9
Tobias <thaegi@gmx.de>
2020-02-10 22:30:44 +0100

    Add notification categories tests

---
+++ tests/Functional/Admin/NotificationCategories/AdminTest.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Tests\Functional\Admin\NotificationCategories;
+
+use Tests\Functional\Base\BaseTestCase;
+
+class AdminTest extends BaseTestCase {
+
+    protected $uri_overview = "/notifications/categories/";
+    protected $uri_edit = "/notifications/categories/edit/";
+    protected $uri_save = "/notifications/categories/save/";
+    protected $uri_delete = "/notifications/categories/delete/";
+
+    protected function setUp(): void {
+        $this->login("admin", "admin");
+    }
+
+    protected function tearDown(): void {
+        $this->logout();
+    }
+
+    public function testList() {
+        $response = $this->request('GET', $this->uri_overview);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('<table id="notifications_categories_table"', $body);
+    }
+
+    public function testGetAddElement() {
+        $response = $this->request('GET', $this->uri_edit);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('<form class="form-horizontal" action="' . $this->uri_save . '" method="POST">', $body);
+
+        return $this->extractFormCSRF($response);
+    }
+
+    /**
+     * @depends testGetAddElement
+     */
+    public function testPostAddElement($csrf_data) {
+
+        $data = [
+            "name" => "Test Notification Category 2",
+            "identifier" => "test_notification_category2"
+        ];
+
+        $response = $this->request('POST', $this->uri_save, array_merge($data, $csrf_data));
+        
+        $body = (string) $response->getBody();
+
+        $this->assertEquals(301, $response->getStatusCode());
+        $this->assertEquals($this->uri_overview, $response->getHeaderLine("Location"));
+
+        return $data;
+    }
+
+    /**
+     * @depends testPostAddElement
+     */
+    public function testAddedElement($data) {
+        $response = $this->request('GET', $this->uri_overview);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $row = $this->getElementInTable($body, $data);
+
+        $this->assertArrayHasKey("id_edit", $row);
+        $this->assertArrayHasKey("id_delete", $row);
+
+        $result = [];
+        $result["id"] = $row["id_edit"];
+        $result["csrf"] = $this->extractJSCSRF($response);
+
+        return $result;
+    }
+
+    /**
+     * Edit created element
+     * @depends testAddedElement
+     */
+    public function testGetElementCreatedEdit(array $result_data) {
+
+        $response = $this->request('GET', $this->uri_edit . $result_data["id"]);
+
+        $body = (string) $response->getBody();
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $this->assertStringContainsString("<input name=\"id\" id=\"entry_id\" type=\"hidden\" value=\"" . $result_data["id"] . "\">", $body);
+
+        $matches = [];
+        $re = '/<form class="form-horizontal" action="(?<save>[\/a-zA-Z0-9]*)" method="POST">.*<input name="id" id="entry_id" type="hidden" value="(?<id>[0-9]*)">/s';
+        preg_match($re, $body, $matches);
+
+        $this->assertArrayHasKey("save", $matches);
+        $this->assertArrayHasKey("id", $matches);
+
+        $result = [];
+        $result["id"] = $matches["id"];
+        $result["csrf"] = $this->extractFormCSRF($response);
+
+        return $result;
+    }
+
+    /**
+     * 
+     * @depends testGetElementCreatedEdit
+     */
+    public function testPostElementCreatedSave(array $result_data) {
+
+        $data = [
+            "id" => $result_data["id"],
+            "name" => "Test Notification Category 2 Updated",
+            "identifier" => "test_notification_category2_update"
+        ];
+
+        $response = $this->request('POST', $this->uri_save . $result_data["id"], array_merge($data, $result_data["csrf"]));
+
+        $this->assertEquals(301, $response->getStatusCode());
+        $this->assertEquals($this->uri_overview, $response->getHeaderLine("Location"));
+
+        return $data;
+    }
+
+    /**
+     * Is the element updated?
+     * @depends testPostElementCreatedSave
+     */
+    public function testGetElementUpdated(array $result_data) {
+        $response = $this->request('GET', $this->uri_overview);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+
+        $row = $this->getElementInTable($body, $result_data);
+
+        $this->assertArrayHasKey("id_edit", $row);
+        $this->assertArrayHasKey("id_delete", $row);
+
+        $result = [];
+        $result["id"] = $row["id_edit"];
+        $result["csrf"] = $this->extractJSCSRF($response);
+
+        return $result;
+    }
+
+    /**
+     * @depends testGetElementUpdated
+     */
+    public function testDeleteElement($result_data) {
+
+        $response1 = $this->request('GET', $this->uri_overview);
+        $csrf = $this->extractJSCSRF($response1);
+
+
+        $response = $this->request('DELETE', $this->uri_delete . $result_data["id"], $csrf);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('{"is_deleted":true,"error":""}', $body);
+    }
+
+    protected function getElementInTable($body, $data) {
+        $matches = [];
+        $re = '/<tr>\s*<td>' . preg_quote($data["name"]) . '<\/td>\s*<td><a href="' . str_replace('/', "\/", $this->uri_edit) . '(?<id_edit>.*)"><span class="fas fa-edit fa-lg"><\/span><\/a><\/td>\s*<td><a href="#" data-url="' . str_replace('/', "\/", $this->uri_delete) . '(?<id_delete>.*)" class="btn-delete"><span class="fas fa-trash fa-lg"><\/span><\/a>\s*<\/td>\s*<\/tr>/';
+        preg_match($re, $body, $matches);
+
+        return $matches;
+    }
+
+}
---
+++ tests/Functional/Admin/NotificationCategories/UserTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Tests\Functional\Admin\NotificationCategories;
+
+use Tests\Functional\Base\BaseTestCase;
+
+class UserTest extends BaseTestCase {
+
+    protected $uri_overview = "/notifications/categories/";
+    protected $uri_edit = "/notifications/categories/edit/";
+    protected $uri_save = "/notifications/categories/save/";
+    protected $uri_delete = "/notifications/categories/delete/";
+    
+    protected $TEST_CATEGORY = 1;
+
+    protected function setUp(): void {
+        $this->login("user", "user");
+    }
+
+    protected function tearDown(): void {
+        $this->logout();
+    }
+
+    public function testList() {
+        $response = $this->request('GET', $this->uri_overview);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('Kein Zugriff erlaubt', $body);
+    }
+
+    public function testGetAddElement() {
+        $response = $this->request('GET', $this->uri_edit);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('Kein Zugriff erlaubt', $body);
+
+        return $this->extractJSCSRF($response);
+    }
+
+    /**
+     * @depends testGetAddElement
+     */
+    public function testPostAddElement($csrf_data) {
+
+        $data = [
+            "name" => "Test Notification Category 2",
+            "identifier" => "test_notification_category2"
+        ];
+
+        $response = $this->request('POST', $this->uri_save, array_merge($data, $csrf_data));
+
+        $this->assertEquals(200, $response->getStatusCode());
+        
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('Kein Zugriff erlaubt', $body);
+    }
+    
+    
+    public function testPostElementCreatedSave() {
+        
+        $response1 = $this->request('GET', $this->uri_overview);
+        $csrf = $this->extractJSCSRF($response1);
+        
+        $data = [
+            "id" => $this->TEST_CATEGORY,
+            "name" => "Test Notification Category 2",
+            "identifier" => "test_notification_category2"
+        ];
+
+        $response = $this->request('POST', $this->uri_save . $this->TEST_CATEGORY, array_merge($data, $csrf));
+
+        $this->assertEquals(200, $response->getStatusCode());
+        
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('Kein Zugriff erlaubt', $body);
+    }
+
+    public function testDeleteElement() {
+
+        $response1 = $this->request('GET', $this->uri_overview);
+        $csrf = $this->extractJSCSRF($response1);
+
+        $response = $this->request('DELETE', $this->uri_delete . $this->TEST_CATEGORY, $csrf);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('Kein Zugriff erlaubt', $body);
+    }
+
+
+}
