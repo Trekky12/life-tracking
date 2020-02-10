6402b36fab3b4700753525a6154b596c9f84b8d4
Tobias <thaegi@gmx.de>
2020-02-10 22:04:01 +0100

    Add Change Password tests

---
+++ tests/Functional/Profile/ChangePWTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Tests\Functional\Profile;
+
+use Tests\Functional\Base\BaseTestCase;
+
+class ChangePWTest extends BaseTestCase {
+
+    protected $uri_overview = "/profile/changepassword";
+    protected $uri_save = "/profile/changepassword";
+
+    public function testOverview() {
+        $this->login("user", "user");
+
+        $response = $this->request('GET', $this->uri_overview);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('<form action="/profile/changepassword" method="POST">', $body);
+
+        $this->logout();
+
+        return $this->extractFormCSRF($response);
+    }
+
+    /**
+     * @depends testOverview
+     */
+    public function testChangePassword($csrf_data) {
+
+        $this->login("user", "user");
+
+        $data = [
+            "oldpassword" => "user",
+            "newpassword1" => "user_new",
+            "newpassword2" => "user_new"
+        ];
+
+        $response = $this->request('POST', $this->uri_save, array_merge($data, $csrf_data));
+
+        $this->assertEquals(301, $response->getStatusCode());
+        $this->assertEquals("/", $response->getHeaderLine("Location"));
+        
+        $this->logout();
+
+        return $data;
+    }
+
+    /**
+     * @depends testChangePassword
+     */
+    public function testChangedPassword($data) {
+
+        $this->login("user", $data["newpassword1"]);
+
+        $response = $this->request('GET', $this->uri_overview);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('<form action="/profile/changepassword" method="POST">', $body);
+
+        $this->logout();
+        
+        return $this->extractFormCSRF($response);
+    }
+
+    /**
+     * @depends testChangedPassword
+     */
+    public function testChangePasswordBack($csrf_data) {
+
+        $this->login("user", "user_new");
+
+        $data = [
+            "oldpassword" => "user_new",
+            "newpassword1" => "user",
+            "newpassword2" => "user"
+        ];
+
+        $response = $this->request('POST', $this->uri_save, array_merge($data, $csrf_data));
+
+        $this->assertEquals(301, $response->getStatusCode());
+        $this->assertEquals("/", $response->getHeaderLine("Location"));
+        
+        $this->logout();
+
+        return $data;
+    }
+    
+    /**
+     * @depends testChangePasswordBack
+     */
+    public function testChangedPasswordBack($data) {
+
+        $this->login("user", $data["newpassword1"]);
+
+        $response = $this->request('GET', $this->uri_overview);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('<form action="/profile/changepassword" method="POST">', $body);
+
+        $this->logout();
+    }
+
+}
---
+++ tests/Functional/Profile/ChangePWWrongOldTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Functional\Profile;
+
+use Tests\Functional\Base\BaseTestCase;
+
+class ChangePWWrongOldTest extends BaseTestCase {
+
+    protected $uri_overview = "/profile/changepassword";
+    protected $uri_save = "/profile/changepassword";
+
+    protected function setUp(): void {
+        $this->login("user", "user");
+    }
+
+    protected function tearDown(): void {
+        $this->logout();
+    }
+
+    public function testOverview() {
+        $response = $this->request('GET', $this->uri_overview);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertStringContainsString('<form action="/profile/changepassword" method="POST">', $body);
+
+        return $this->extractFormCSRF($response);
+    }
+
+    /**
+     * @depends testOverview
+     */
+    public function testChangePasswordWrongOld($csrf_data) {
+
+        $data = [
+            "oldpassword" => "user1",
+            "newpassword1" => "user_new",
+            "newpassword2" => "user_new"
+        ];
+
+        $response = $this->request('POST', $this->uri_save, array_merge($data, $csrf_data));
+
+        $body = (string) $response->getBody();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString("Altes Passwort ist falsch", $body);
+    }
+
+}
