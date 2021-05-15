f883ad5f0ccde030fdf3bf0c946fabb9fca1ee34
Tobias <thaegi@gmx.de>
2021-05-15 14:15:06 +0200

    Update dependencies

--- src/Domain/User/Profile/QRCodeProvider.php
+++ src/Domain/User/Profile/QRCodeProvider.php
@@ -1,23 +1,28 @@
 <?php
+
 namespace App\Domain\User\Profile;
+
 use RobThree\Auth\Providers\Qr\IQRCodeProvider;
 use Endroid\QrCode\QrCode;
-use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelMedium;
+use Endroid\QrCode\Writer\PngWriter;
 
-class QRCodeProvider implements IQRCodeProvider{
-    
+class QRCodeProvider implements IQRCodeProvider {
+
     public function getMimeType() {
-    return 'image/png';
+        return 'image/png';
     }
 
     public function getQRCodeImage($qrtext, $size) {
         $qrCode = new QrCode($qrtext);
         $qrCode->setSize($size);
         $qrCode->setMargin(0);
-        $qrCode->setErrorCorrectionLevel(ErrorCorrectionLevel::MEDIUM());
+        $qrCode->setErrorCorrectionLevel(new ErrorCorrectionLevelMedium());
+
+        $writer = new PngWriter();
+        $result = $writer->write($qrCode);
         
-        return $qrCode->writeString();
+        return $result->getString();
     }
 
 }
-
