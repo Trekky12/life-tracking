511e69c7f4b87c331e3ca2f6fdf58acfd252806e
Trekky12 <thaegi@gmx.de>
2025-08-05 18:48:29 +0200

    fix(timesheets): export overview was generating too many items (another time)
    see commit fb6d6ed08b500cccc22963e6d44115845c911673

--- src/Domain/Timesheets/Sheet/SheetMapper.php
+++ src/Domain/Timesheets/Sheet/SheetMapper.php
@@ -712,7 +712,7 @@ class SheetMapper extends \App\Domain\Mapper {
             $bindings["customer"] = $customer;
         }
 
-        $select = "COUNT(t.id) as count, tcus.name as customerName, tcus.id as customer";
+        $select = "COUNT(DISTINCT t.id) as count, tcus.name as customerName, tcus.id as customer";
         list($tableSQL, $cat_bindings) = $this->getTableSQL($select, $categories, $include_empty_categories, $invoiced, $billed, $payed, $happened, $customer, 'tcus.id');
 
         $sql = $tableSQL;
